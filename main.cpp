901c6af1dae761deca52be2945a3a80b223149d6
Barun Kumar <zen.barun.ith@gmail.com>
2025-03-05 01:58:48 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <windows.h>
+#include<vector>
+#include<sstream>
+
+/*to do for tommorow
+1.add an auto fill option 
+2.add default windows command 
+3.make a gui
+*/
+using namespace std;
+
+vector<string> parseIn(const string &input)
+{
+    vector<string> tokens;
+    istringstream stream(input);
+    string token;
+
+    while(stream>>token)
+    {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+void executeCommand(const vector<string>& args,vector<string> &prefix)
+{
+    if(args.empty())return;
+    
+    if(args[0]=="cd")
+    {
+        if(args.size()<2)
+        {
+            cerr<<"Error: Missing Directory\n";
+        }
+        else{
+            if(!SetCurrentDirectory(args[1].c_str()))
+            {
+                cerr<<"Error: Could not change directory\n";
+            }
+            else{
+                prefix.push_back(args[1]);
+                prefix.push_back("/");
+            }
+        }
+        return;
+    }
+    else if(args[0]=="cls")
+    {
+        system("cls");
+        return;
+    }
+
+    string commandLine; //converting string to char * for create process
+    for(const string&arg:args)
+    {
+        commandLine += arg+" ";//concatanation ["this","is","a","command"]->"this is a command";
+    }
+
+    STARTUPINFO si={sizeof(STARTUPINFO)};
+    PROCESS_INFORMATION pi;
+    char cmd[1024];
+    strcpy_s(cmd,commandLine.c_str());
+
+    if(CreateProcess(NULL,cmd,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi)){
+        WaitForSingleObject(pi.hProcess,INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+    else{
+        cerr<<"Error:Command not found.\n";
+    }
+}
+
+int main(void)
+{
+    system("cls");
+    string input;
+    cout<<"Type help -c  for possible command\n";
+    vector<string> shellPrefix;
+    shellPrefix.push_back("$");
+    while(true)
+    {
+    for(auto itr:shellPrefix)
+    {
+        cout<<itr;
+    }
+    getline(cin,input);
+
+    if(input =="ez"||input=="exit")
+    {
+        break;
+    }
+    vector<string> parsed=parseIn(input);
+    executeCommand(parsed,shellPrefix);
+    }
+    return 0;
+
+}
